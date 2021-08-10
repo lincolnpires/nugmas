ce7d29ca82ed1d1f4ce54a83c93392ca63b7bb47
Lincoln Pires <lincolnpires@users.noreply.github.com>
2021-08-10 23:43:31 +0200

    Remove unecessary boilerplate code

--- Home.cs
+++ Home.cs
@@ -24,17 +24,6 @@ namespace Nugmas.FunctionSite
             var stream = File.OpenRead($"{wwwroot}/index.html");
             var result = new FileStreamResult(stream, "text/html");
             return result;
-
-
-            string name = req.Query["name"];
-
-            var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name ??= data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
 }
